52978c2e5944c22a5cbbb2a258b3a8a5c5cf186c
Duncan Marsh <dwolfm@gmail.com>
2017-09-21 14:02:18 -0700

    added solution description

--- 00-whiteboard-solutions/09-traverse-linked-list.js
+++ 00-whiteboard-solutions/09-traverse-linked-list.js
@@ -1,5 +1,17 @@
 'use strict'
 
+// traverse the following data and print out each value
+// let data = {
+//   value: 3, 
+//   next: {
+//     value: 8, 
+//     next: {
+//       value: 11, 
+//       next: null,
+//     },
+//   },
+// }
+
 const solutionA = (list) => {
   while(list){
     console.log(list.value)
